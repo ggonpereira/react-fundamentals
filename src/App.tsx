4c87c43e123e56c986ef24ce4a3232397b698386
Gabriel Pereira <ggonpereira@gmail.com>
2022-10-15 18:55:54 -0300

    📦 refactor: direct return on App component

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ import { Home } from "./pages/Home";
 import { GlobalStyle } from "./styles/global";
 import { themes } from "./styles/themes";
 
-export const App = () => {
-  return (
-    <ThemeProvider theme={themes.dark}>
-      <GlobalStyle />
-      <Home />
-    </ThemeProvider>
-  );
-};
+export const App = () => (
+  <ThemeProvider theme={themes.dark}>
+    <GlobalStyle />
+    <Home />
+  </ThemeProvider>
+);
