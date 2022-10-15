eba06ee5403402b895542966ab6a575209083f38
Gabriel Pereira <ggonpereira@gmail.com>
2022-10-15 13:32:12 -0300

    💎 style: map colors and add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
+import { ThemeProvider } from "styled-components";
 import { Home } from "./pages/Home";
 import { GlobalStyle } from "./styles/global";
+import { themes } from "./styles/themes";
 
 export const App = () => {
   return (
-    <>
+    <ThemeProvider theme={themes.dark}>
       <GlobalStyle />
       <Home />
-    </>
+    </ThemeProvider>
   );
 };
---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,18 @@
+export const dark = {
+  colors: {
+    gray: {
+      800: "#121214",
+      700: "#202024",
+      600: "#323238",
+      500: "#7C7C8A",
+      300: "#8D8D99",
+      200: "#C4C4CC",
+      100: "#E1E1E6",
+    },
+    white: "#FFFFFF",
+    black: "#000000",
+    danger: "#F75A68",
+    primary: "#E21B1B",
+    secondary: "#FC4040",
+  },
+};
