288905c85ee71ca2bcef5b6e62180d8f8e52a375
Gabriel Pereira <ggonpereira@gmail.com>
2022-10-15 13:30:50 -0300

    💎 style: add logo svg icon

---
+++ src/assets/icons.tsx
@@ -0,0 +1,22 @@
+export const Logo = () => (
+  <svg
+    version="1.1"
+    id="Layer_1"
+    xmlns="http://www.w3.org/2000/svg"
+    xmlnsXlink="http://www.w3.org/1999/xlink"
+    x="0px"
+    y="0px"
+    viewBox="0 0 512 512"
+    xmlSpace="preserve"
+  >
+    <polygon points="68,373.776 0,256 68,138.224 204,138.224 272,256 204,373.776 " />
+    <polygon
+      style={{ fill: "#E21B1B" }}
+      points="308,235.56 240,117.776 308,0 444,0 512,117.776 444,235.56 "
+    />
+    <polygon
+      style={{ fill: "#666666" }}
+      points="308,512 240,394.224 308,276.44 444,276.44 512,394.224 444,512 "
+    />
+  </svg>
+);
