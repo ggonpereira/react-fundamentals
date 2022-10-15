7994d294b39b0071a943d9e9345b7ecb63f33235
Gabriel Pereira <ggonpereira@gmail.com>
2022-10-15 14:37:56 -0300

    💎 style: reset on CSS and add default color to global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,7 +1,18 @@
 import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0px;
+    padding: 0px;
+    box-sizing: border-box;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
   body {
     font-family: 'Roboto', sans-serif;
+    color: ${({ theme }) => theme.colors.gray[7]}
   }
 `;
