5027fb9f8282aefa65c25fa697155a77a003b9b1
Gabriel Pereira <ggonpereira@gmail.com>
2022-10-15 23:55:43 -0300

    👌 improve: controlling TextArea component

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -2,8 +2,28 @@ import * as S from "./TextArea.styles";
 
 interface TextAreaProps {
   rows?: number;
+  placeholder?: string;
+  value: string;
+  onChange: React.Dispatch<React.SetStateAction<string>>;
 }
 
-export const TextArea = ({ rows = 3 }: TextAreaProps) => (
-  <S.TextArea rows={rows} />
-);
+export const TextArea = ({
+  rows = 3,
+  placeholder,
+  value,
+  onChange,
+}: TextAreaProps) => {
+  const handleOnChange = (e: React.FormEvent<HTMLTextAreaElement>) => {
+    const target = e.target as HTMLTextAreaElement;
+    onChange(target.value);
+  };
+
+  return (
+    <S.TextArea
+      rows={rows}
+      placeholder={placeholder}
+      value={value}
+      onChange={handleOnChange}
+    />
+  );
+};
