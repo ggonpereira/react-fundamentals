81acfe235fa153e4186862628e43796bdb216702
Gabriel Pereira <ggonpereira@gmail.com>
2022-10-15 23:57:48 -0300

    👌 improve: add id to posts

--- src/constants/posts.ts
+++ src/constants/posts.ts
@@ -1,5 +1,7 @@
 export const posts = [
-  `
+  {
+    id: 1,
+    content: `
   <p>Fala galeraa 👋</p>
   <br />
   <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
@@ -7,4 +9,5 @@ export const posts = [
  <p>👉 <a href="jane.design/doctorcare">jane.design/doctorcare</a></p>
   <br />
   <a href="/hashtag/novoproject">#novoprojeto</a> <a href="/hashtag/nlw">#nlw</a> <a href="/hashtag/rocketseat">#rocketseat</a>`,
+  },
 ];
