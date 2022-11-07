18f955970e9c94345eb2ab70d66acc597163fcd2
Gabriel Pereira <ggonpereira@gmail.com>
2022-11-07 00:44:16 -0300

    👌 improve: Button now receiving 'type' props

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,10 +3,11 @@ import * as S from "./Button.styles";
 
 interface ButtonProps {
   variant?: "filled" | "outlined";
-  onClick: (prop?: any) => void;
+  onClick?: (prop?: any) => void;
   icon?: ReactNode;
   children: ReactNode;
   isFullWidth?: boolean;
+  type: "submit" | "button";
 }
 
 export const Button = ({
@@ -15,8 +16,14 @@ export const Button = ({
   icon,
   children,
   isFullWidth,
+  type = "button",
 }: ButtonProps) => (
-  <S.Button onClick={onClick} variant={variant} isFullWidth={isFullWidth}>
+  <S.Button
+    onClick={onClick}
+    variant={variant}
+    isFullWidth={isFullWidth}
+    type={type}
+  >
     {icon && icon}
 
     <S.Text>{children}</S.Text>
