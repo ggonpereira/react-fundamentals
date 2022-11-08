b1a9887e14983c3764127a3abf76920f01980e93
Gabriel Pereira <ggonpereira@gmail.com>
2022-11-08 13:23:22 -0300

    📦 refactor: create TextArea inside Post.styles

--- src/components/Post/Post.styles.tsx
+++ src/components/Post/Post.styles.tsx
@@ -81,3 +81,24 @@ export const FeedbackArea = styled.form`
     }
   }
 `;
+
+export const TextArea = styled.textarea`
+  background: ${({ theme }) => theme.colors.gray[900]};
+  border-radius: 8px;
+  border: 0.1rem solid ${({ theme }) => theme.colors.primary};
+  color: ${({ theme }) => theme.colors.gray[300]};
+  font-size: 1.6rem;
+  line-height: 2.24rem;
+  max-height: 20rem;
+  min-height: 9.5rem;
+  outline: none;
+  padding: 1.3rem 1.6rem;
+  resize: vertical;
+  transition: border 0.1s ease-in;
+  width: 100%;
+
+  &:focus {
+    border-color: ${({ theme }) => darken(theme.colors.primary, 0.17)};
+    box-shadow: 0px 0px 14px 0px rgba(251, 167, 31, 0.027);
+  }
+`;
